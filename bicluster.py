24344c4e6ac26069b074931623c13cef229ef4ff
Andrew Kelleher <akellehe@gmail.com>
2012-04-20 22:22:41 -0400

    Got accurate running version

--- bicluster.py
+++ bicluster.py
@@ -12,214 +12,152 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
-
 import numpy
 import random
-from itertools import combinations
+import operator
+import time
+from math import *
 
-# A, k, m
-# B, l, n
+#A, k, m rows
+#B, l, n cols
 
+ITERATIONS = 1000
+SIZE = 200
+SHAPE = ( SIZE, SIZE )
 
-# Define a stability threashold 
-STABILITY_THREASHOLD = 1000 
-
-# Define the size and shape of the test matrix.
-SIZE  = 10
-SHAPE = ( SIZE, SIZE ) 
-
-# Populate the test matrix with gaussian noise on (0,1)
+# Populate a test matrix with gaussian noise in (0,1)
 X = numpy.random.normal( size=SHAPE )
-X += 1
+X += 2
 X *= 0.5
 
 # Add a feature to detect
 FEATURE = numpy.zeros( SHAPE )
-FEATURE[0:SIZE/2,0:SIZE/2] = 0.5
+FEATURE[0:SIZE/2,0:SIZE/2] = 4
 X += FEATURE
 
-# Get m and n
-m, n = X.shape # rows, columns
+print X
 
+m, n = X.shape
 
 # Select initial k and l from {1...[m/2]} and {1...[n/2]} respectively (at random)
-def get_k_and_l( ):
+def get_k_and_l( m, n ):
     k = random.randint( 1, int( m/2 ) )
     l = random.randint( 1, int( n/2 ) )
     return ( k, l )
 
-# Start with a random B ( matrix of l cols )
-def get_B_candidate_at_random( X, l, n, sets_checked ):
+
+def get_initial_B( l, n, X ):
     l_zeros = numpy.zeros( ( 1, l ), dtype=int )
     l_rands = numpy.random.random( ( 1, l ) )
     l_rands *= n
     l_zeros += l_rands
     which_l_cols = l_zeros
-
-    sets = 0
-    while tuple( which_l_cols[ 0 ] ) in sets_checked:
-        l_zeros = numpy.zeros( ( 1, l ), dtype=int )
-        l_rands = numpy.random.random( ( 1, l ) )
-        l_rands *= n
-        l_zeros += l_rands 
-        which_l_cols = l_zeros
-        sets += 1
-        if sets >= STABILITY_THREASHOLD:
-            return False 
-        print "Fetching unchecked set..."
-    
-    sets_checked.append( tuple( which_l_cols[ 0 ] ) )
-
-    B = numpy.zeros( X.shape ) 
+    B = numpy.zeros( X.shape )
     for i in which_l_cols:
         B[:,i] = X[:,i]
-
-    return B 
-
-# Find the best A ( matrix of k rows ) for the chosen B
-def get_A_candidate_at_random( X, k, m, sets_checked ):
-    k_zeros = numpy.zeros( ( 1, k ), dtype=int )
-    k_rands = numpy.random.random( ( 1, k ) )
-    k_rands *= m
-    k_zeros += k_rands
-    which_k_rows = k_zeros 
-
-    sets = 0
-    while tuple( which_k_rows[ 0 ] ) in sets_checked: 
-        k_zeros = numpy.zeros( ( 1, k ), dtype=int )
-        k_rands = numpy.random.random( ( 1, k ) )
-        k_rands *= m
-        k_zeros += k_rands
-        which_k_rows = k_zeros 
-        sets += 1
-        if sets >= STABILITY_THREASHOLD:
-            return False
-        print "Fetching unchecked set..." 
-    
-    sets_checked.append( tuple( which_k_rows[ 0 ] ) )
-   
-    A = numpy.zeros( X.shape )
-    for i in which_k_rows:
-        A[i,:] = X[i,:]
-
-    return A 
-        
-def score_A_and_B( A, B, X ):
-    C = A * B
-    indicies = C.nonzero( )
-    entries = A[ indicies ]
-    total = numpy.sum( entries )
-    return total
-     
-def get_A( k, l, B, X ):
-    running_max = 0
-    winner = None
-    a_sets_checked = [ ]
-    for iteration in range( 0, STABILITY_THREASHOLD / 10 ):
-        A = get_A_candidate_at_random( X, k, m, a_sets_checked )
-        total = score_A_and_B( A, B, X )
-        if total > running_max:
-            running_max = total
-            winner = A
-    return winner
-
-def get_B( k, l, A, X ):
-    running_max = 0
-    winner = None
-    b_sets_checked = [ ]
-    for iteration in range( 0, STABILITY_THREASHOLD / 10 ):
-        B = get_B_candidate_at_random( X, l, n, b_sets_checked )
-        total = score_A_and_B( A, B, X )
-        if total > running_max:
-            running_max = total
-            winner = B
-    return winner
-
-def get_best_A( k, l, B, X ):
-    running_max = 0
-    winner = None
-    combos = combinations( range( 0, m ), k )  
-    for combo in combos:
-        print combo
-
-def have_converged( A, B ):
-    return sum( A - B ) == 0
-
-def build_matrix_from_k_rows_of_X( k_rows, X ):
-    M = numpy.zeros( X.shape )
-    for row in k_rows:
-        M[row,:] = X[row,:]
-    return M
-
-def build_matrix_from_l_cols_of_X( l_cols, X ):
-    M = numpy.zeros( X.shape )
-    for col in l_cols:
-        M[:,col] = X[:,col]
-    return M
-
-def get_k_rows_with_largest_sum_over_columns_of_B( k, B ):
-    column_list = range( 1, m )
-    winning_total = 0
-    winner = None
-    for max_candidate in combinations( column_list, k ):
-        A = build_matrix_from_k_rows_of_X( max_candidate, X )
-        score = sum_rows_of_A_over_cols_of_B( A, B )
-        if score > winning_total:
-            winner = A
-            winning_total = score
-    return A
-
-
-def get_l_columns_with_largest_sum_over_rows_of_A( l, A ):
-    row_list = range( 1, n )
-    winning_total = 0
-    winner = None
-    for max_candidate in combinations( row_list, l ):
-        B = build_matrix_from_l_cols_of_X( max_candidate, X )
-        score = sum_cols_of_B_over_rows_of_A( A, B )
-        if score > winning_total:
-            winner = B
-            winning_total = score
     return B
 
-def get_intersection( A, B, X ):
-    intersection = numpy.zeros( X.shape )
+def get_k_rows_with_largest_sum_over_B( k, B ):
+    sums = [ ]
+    for row in B:
+        sums.append( sum( row ) )
+
+    best_rows = [ ]
+    # Row indicies sorted lowest to highest:
+    inds = [ i for ( i, j ) in sorted( enumerate( sums ), key = operator.itemgetter( 1 ) ) ]
+    for i in range( 0, k ):
+        best_rows.append( inds.pop( ) )
+
+    return best_rows
+
+def get_A_from_row_indicies( rows, X ):
+    A = numpy.zeros( X.shape )
+    for row_index in rows:
+        A[row_index,:] = X[row_index,:]
+    return A
+
+def get_l_cols_with_largest_sum_over_A( l, A ):
+    sums = [ ]
+    for col in A.transpose( ):
+        sums.append( sum( col ) )
+
+    best_cols = [ ]
+    # Col indicies sorted from lowest to highest:
+    inds = [ i for ( i, j ) in sorted( enumerate( sums ), key = operator.itemgetter( 1 ) ) ]
+    for i in range( 0, l ):
+        best_cols.append( inds.pop( ) )
+
+    return best_cols
+
+def get_B_from_col_indicies( cols, X ):
+    B = numpy.zeros( X.shape )
+    for col_index in cols:
+        B[:,col_index] = X[:,col_index]
+    return B
+
+def have_converged( newA, newB, oldA, oldB ):
+    a_converged = ( sum( oldA ) - sum( newA ) ).all( ) == 0 
+    b_converged = ( sum( oldB ) - sum( newB ) ).all( ) == 0 
+    if a_converged and b_converged:
+        return True
+    else:
+        return False
+   
+def get_intersection( A, B ):
+    intersection = numpy.zeros( A.shape )
     C = A * B
     indicies = C.nonzero( )
     intersection[ indicies ] = A[ indicies ]
-    return intersection 
-    
+    return intersection
 
-def get_submatrix( X, k=None, l=None ):
-    stability_counter = 0
-    highest_score = -100000
-    k, l = get_k_and_l( )
-    a_sets_checked = [ ]
-    b_sets_checked = [ ]
-    B = get_B_candidate_at_random( X, l, n, b_sets_checked )
-    while stability_counter < STABILITY_THREASHOLD:
-        print "Iterating... " + str( stability_counter )
-        A_candidate = get_A_candidate_at_random( X, k, m, a_sets_checked )
-        if A_candidate is False:
-            return get_intersection( A, B, X )
-        #A_candidate = get_A( k, l, B, X )
-        score = score_A_and_B( A_candidate, B, X )
-        if score > highest_score:
-            highest_score = score
-            A = A_candidate.copy( )
-            stability_counter = 0
-        B_candidate = get_B_candidate_at_random( X, l, n, b_sets_checked )
-        if B_candidate is False:
-            return get_intersection( A, B, X )
-        #B_candidate = get_B( k, l, A, X )
-        score = score_A_and_B( A, B_candidate, X )
-        if score > highest_score:
-            highest_score = score
-            B = B_candidate.copy( )
-            stability_counter = 0
-        stability_counter += 1
-    print "k, l: " + str( k ) + ", " + str( l )
-    return get_intersection( A, B, X )
+def binomial( n, k ):
+    if n > k:
+        b = factorial( n ) / ( factorial( k ) * factorial( n - k ) )
+        return b
+
+def S( U, k, l, m, n ):
+    tau = numpy.average( U )
+
+    first_term  = -log( binomial( m, k ) * binomial( n, l ), 2 )
+    second_term = ( ( tau**2.0 ) * k * l ) / ( 2.0 * log( 2.0 ) )
+    third_term  = -log( ( tau**2.0 * k * l ), 2 ) / 2.0
+
+    return log( 2.0 ) * ( first_term + second_term + third_term ) 
+
+def get_remaining_time( start_time, total_time, iteration ):
+    if iteration == 0:
+        return ""
+    elapsed_time = total_time - start_time
+    time_per_iteration = elapsed_time / iteration
+    return str( time_per_iteration * ( ITERATIONS - iteration ) / 60 ) + " minutes remaining"
 
 if __name__ == '__main__':
-    print get_submatrix( X )
+    winner = numpy.zeros( X.shape )
+    winning_score = 0 
+    end_time = False
+    start_time = time.time( )
+    for it in range( 0, ITERATIONS ):
+        k, l = get_k_and_l( m, n )
+        B = get_initial_B( l, n, X )
+        lastA = numpy.zeros( X.shape )
+        lastB = numpy.zeros( X.shape )
+        while True: 
+            a_inds = get_k_rows_with_largest_sum_over_B( k, B )
+            A = get_A_from_row_indicies( a_inds, X )
+            b_inds = get_l_cols_with_largest_sum_over_A( l, A )
+            B = get_B_from_col_indicies( b_inds, X )
+            if have_converged( A, B, lastA, lastB ):
+                break
+            lastA = A.copy( )
+            lastB = B.copy( )
+        U = get_intersection( A, B )
+        U_score = S( U, k, l, m, n )
+        if U_score > winning_score:
+            winner = U.copy( )
+            winning_score = U_score
+
+        print get_remaining_time( start_time, time.time( ), it )
+
+
+    print winning_score
+    print winner
